5ddc7275d92bbb49aa68859f6890211305955b4a
Rafael Neves <78914355+NevesRafa@users.noreply.github.com>
2022-07-29 11:21:41 -0300

    - Feito tela de informações

---
+++ app/src/main/java/com/nevesrafael/filmland/InfoScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.nevesrafael.filmland
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InfoScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_screen)
+    }
+}
